e7811b49f6c0b17632f75446c583d31475d03ea5
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2024-03-31 02:31:27 +0530

    Created a seperate folder & added 4 java files to it

---
+++ Basics1/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int rollNo = 1662;
+        char letter = 'D';
+        float marks = 98.67f;
+        double largeDecimanNumbers = 345345345.453543;
+        long largeIntegers = 254346546;
+        boolean check = false;
+
+    }
+}
---
+++ Basics1/TypeCasting.java
@@ -0,0 +1,11 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        byte b = 42;
+        char c = 'c';
+        short s = 1023;
+        int i = 5000;
+        float f = 5.67f;
+        double d = 0.12345;
+        
+    }
+}
