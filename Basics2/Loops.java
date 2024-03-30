4536a6a5d34cfd307c8a6dba143af01e50996e12
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2024-03-31 03:09:18 +0530

    Modified print statement

--- Basics2/Loops.java
+++ Basics2/Loops.java
@@ -3,7 +3,7 @@ package Basics2;
 public class Loops {
     public static void main(String[] args) {
         for(int i = 0; i < 10; i++) {
-            System.out.println(i);
+            System.out.println(i + 1);
         }
     }
 }
